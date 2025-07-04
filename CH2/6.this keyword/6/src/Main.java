8d8fe9427d489ebb96a4eea18dec3f5cff9aa279
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-04 09:12:22 +0900

    ch6創建

---
+++ CH2/6.this keyword/6/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(10);
+        c1.setI();
+        c1.accessI();
+    }
+}
