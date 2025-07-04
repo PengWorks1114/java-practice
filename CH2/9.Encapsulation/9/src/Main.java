a3cdb31911644f16bfb7a1660fb3e7242ab70688
peng0o0 <tomato.202504.penghc@gmail.com>
2025-07-04 11:19:57 +0900

    CH9更改

--- CH2/9.Encapsulation/9/src/Main.java
+++ CH2/9.Encapsulation/9/src/Main.java
@@ -2,14 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    Robot r = new Robot("Buzz", 1995);
+    r.yearMade = -500;
+    //隨時可以改它裡面的數值 因為我在Robot class裡面給他設定為public
+    //為了不讓這件事情發生 我們要去更改Robot class裡面改為private
+        System.out.println(r.name);
+        System.out.println(r.yearMade);
     }
 }
---
+++ CH2/9.Encapsulation/9/src/Robot.java
@@ -0,0 +1,9 @@
+public class Robot {
+    public String name;
+    public int yearMade;
+
+    public Robot(String naem, int yearMade) {
+        this.name = naem;
+        this.yearMade = yearMade;
+    }
+}
